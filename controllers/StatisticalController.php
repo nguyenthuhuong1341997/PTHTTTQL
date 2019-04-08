a49428b630bb2dd923c141c76e70ef789f4d4bab
Huong <nguyenthuhuong1341997@gmail.com>
2019-04-09 01:11:04 +0700

    chart spline

--- controllers/StatisticalController.php
+++ controllers/StatisticalController.php
@@ -1,6 +1,7 @@
 <?php 
 	include_once 'models/Statistical.php';
 	include_once 'models/Site.php';
+	include_once 'models/Order.php';
 
 	/**
 	 * 
@@ -9,16 +10,31 @@
 	{
 		var $statistical_model;
 		var $site_model;
+		var $order_model;
 		function __construct()
 		{
 			$this->statistical_model = new Statistical();
 			$this->site_model = new Site();
+			$this->order_model = new Order();
 		}
 
 		public function index()
 		{
-			$revenueSites = $this->statistical_model->revenueSite();
+			$years = $this->order_model->getYear();
 			$sites = $this->site_model->list();
+			$year = $years[0]['year'];
+			$site_id = key($sites);
+
+			if(isset($_POST['revenue'])){
+				$year = $_POST['year'];
+				$site_id = $_POST['site_id'];
+			}
+			$revenueSites = $this->statistical_model->revenueSite();
+			$revenueYears = $this->statistical_model->revenueYear($site_id, $year);
+
+			// print_r($revenueYears);
+			// die();
+
 			require_once('views/admin/statistical/index.php');
 		}
 
